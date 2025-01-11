c00826bd996115b9cf8e3dcd6dc1acd105e77cc8
GokaiInfinity <patriciofilbert@gmail.com>
2025-01-11 10:45:56 +0700

    added mbtiresult.js

---
+++ resources/js/mbtiresult.js
@@ -0,0 +1,188 @@
+[
+    {
+        "mbti": "INTJ",
+        "description": "You love thinking and experimenting with new ideas. You’re very independent and like to analyze theory and numbers. Because you love deep thinking, you prefer positions that allow you to innovate.",
+        "careers":[
+            "Project Management",
+            "System Engineering",
+            "Marketing Strategy",
+            "System Analysis",
+            "Software Engineering",
+            "Financial Analysis",
+        ]
+    },
+    {
+        "mbti": "INFJ",
+        "description": "This personality type is very rare - only 1% of the population are INFJs. You approach work with deep thoughtfulness, care, and imagination. You care deeply about others, but you are not a dreamer. You take actionable steps to make the world a better place.",
+        "careers":[
+            "Counselor",
+            "Psychologist",
+            "Human Resources",
+            "Writing/Editing",
+            "Environmental Science",
+            "Special Education",
+        ]
+    },
+    {
+        "mbti": "ISTJ",
+        "description": "You are rational and don’t like making impulsive decisions. You make for a great employee because you are reliable, objective, and sharp. You respect authority and want a sense of security and consistency from your career.",
+        "careers":[
+            "Military & Police",
+            "Legal counsel",
+            "Supply Chain Management",
+            "Medical",
+            "Inspection (of any kind)",
+            "Financial management",
+            "Data analysis",
+        ]
+    },
+    {
+        "mbti": "ISFJ",
+        "description": "You are efficient and responsible but have a great desire to help others. Your personality traits are essential to the modern world - you are altruistic and skilled, so you usually find yourself happy in the service of others.",
+        "careers":[
+            "Nursing",
+            "Teaching",
+            "Social work",
+            "Religious work",
+            "Customer service",
+            "Human resources",
+        ]
+    },
+    {
+        "mbti": "ISTP",
+        "description": "You are highly creative and have an individualistic mindset - stopping at nothing to achieve your goals. You thrive in the unknown and are a born problem-solver, although the problems you want to solve might not be so practical. This combination of curiosity and grit make ISFJs one of the most unique personality types out there.",
+        "careers":[
+            "Mechanics",
+            "Engineering",
+            "Forensic science",
+            "Sports/Athletics",
+            "Intelligence agencies",
+            "Gastronomy",
+            "Detective work",
+        ]
+    },
+    {
+        "mbti": "ISFP",
+        "description": "You love adventures and living in the moment. You use aesthetics and design to fuel your love for beauty. You don’t care much about wealth, power, or security. In your job, you are looking for creative freedom and a chance to express yourself artistically.",
+        "careers":[
+            "Fashion design",
+            "Cosmetology ",
+            "Graphic design",
+            "Music",
+            "Fitness training",
+            "Art director",
+            "Photography",
+        ]
+    },
+    {
+        "mbti": "ENTJ",
+        "description": "You make decisions quickly and are motivated by external rewards. You usually take charge of teams and have high self-confidence. You make sure to exercise your authority so that everyone is doing what they’re supposed to and things are getting done.",
+        "careers":[
+            "Top executive ",
+            "Emergency management",
+            "Entrepreneurship",
+            "Corporate strategy",
+            "Politics",
+        ]
+    },
+    {
+        "mbti": "ENTP",
+        "description": "You are bold, creative, and sarcastic, and are not afraid of hard work to reach your goals. You have a quick wit and think well on your feet, which makes you a great debater. At work, you enjoy creating solutions for technical and intellectual problems.",
+        "careers":[
+            "Stock trading",
+            "Sales",
+            "Film production",
+            "Public relations",
+            "Legal counseling ",
+            "Engineering",
+        ]
+    },
+    {
+        "mbti": "ENFJ",
+        "description": "You love helping others and being the center of attention. You have strong ideas and values and are not afraid to stand for them. You reach your meaningful goals through creative energy and collaboration.",
+        "careers":[
+            "Teaching",
+            "Counseling ",
+            "Human resources",
+            "Politics",
+            "Life coaching",
+            "Motivational speaking",
+        ]
+    },
+    {
+        "mbti": "ENFP",
+        "description": "You are a true free spirit - charming, independent, and energetic. You enjoy interacting with others and exploring new ideas and want your work life to reflect that. You find EVERYTHING interesting, so you can have a hard time choosing a career path.",
+        "careers":[
+            "Sales management",
+            "Real estate",
+            "Customer service",
+            "Film direction",
+            "Screenwriting",
+            "Marketing",
+        ]
+    },
+    {
+        "mbti": "ESTJ",
+        "description": "You are a true leader and honor tradition and order. You have a strong moral compass and enjoy leading others and being busy. You gravitate towards careers that give you a sense of structure and organization, where you can exercise your responsibility and loyalty.",
+        "careers":[
+            "Sales engineering",
+            "Credit analysis",
+            "Insurance",
+            "Real estate",
+            "Corporate management",
+            "Corporate executive",
+            "Public administration",
+        ]
+    },
+    {
+        "mbti": "INFJ",
+        "description": "This personality type is very rare - only 1% of the population are INFJs. You approach work with deep thoughtfulness, care, and imagination. You care deeply about others, but you are not a dreamer. You take actionable steps to make the world a better place.",
+        "careers":[
+            "Counselor",
+            "Psychologist",
+            "Human Resources",
+            "Writing/Editing",
+            "Environmental Science",
+            "Special Education",
+        ]
+    },
+    {
+        "mbti": "ESTP",
+        "description": "You are energetic, goal-oriented, and are inspired by short-term achievements. You want to be where the action is - making decisions daily while thinking on your feet. These are the most popular of the personality types, and they have an easy time networking and interacting with others.",
+        "careers":[
+            "Entrepreneurship",
+            "Management",
+            "Politics",
+            "Real estate",
+            "Acting",
+            "Front office clerk",
+            "In-the-field reporting",
+            "Human resources",
+        ]
+    },
+    {
+        "mbti": "ESFJ",
+        "description": "Like all extroverts, you enjoy interacting with others and taking part in your community. You are an altruist and love to be of service to others. You are extremely loyal and well-organized and avoid conflict at all costs. You are happy to do what needs to be done and don’t shy away from routines. ",
+        "careers":[
+            "Event planning",
+            "Nursing",
+            "Office management",
+            "Paralegal",
+            "Medical assistance",
+            "Receptionist",
+            "Catering",
+        ]
+    },
+    {
+        "mbti": "ESFP",
+        "description": "As an ESFP, you are definitely THE life of the party. You randomly start singing and dancing and love giving out time and energy to other people. Any job that restrains you from that freedom will 100% make you miserable. You want excitement, interaction, and a chance to shine!",
+        "careers":[
+            "Event planning",
+            "Sales",
+            "Trip planning",
+            "Tour guides",
+            "Stand-up comedy",
+            "Theatre",
+        ]
+    },
+]
